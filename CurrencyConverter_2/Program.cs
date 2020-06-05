9e97b34c237a35dbbe3a52cf810c4dca950d0465
kamilwojtysiak <kamil@futrzane.pl>
2020-06-05 08:56:30 +0200

    Poprawiony konwerter

---
+++ CurrencyConverter_2/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace CurrencyConverter_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Currency EUR = new Currency(2321.3, '$', "EUR");
+            Currency USD = new Currency(123.3, '$', "USD");
+
+            CurrencyConverter newConverter = new CurrencyConverter();
+            newConverter.Converter(EUR);
+            newConverter.Converter(USD);
+        }
+    }
+    public class Currency
+    {
+        public double Rate { get; set; }
+        public char CurrencySymbol { get; set; }
+        public string CurrencyName { get; set; }
+
+        public Currency(double rate, char currencySymbol, string currencyName)
+        {
+            this.Rate = rate;
+            this.CurrencySymbol = currencySymbol;
+            this.CurrencyName = currencyName;
+        }
+    }
+    public class CurrencyConverter
+    {
+        public void Converter(Currency currency)
+        {
+            Console.Write("Wprowadź kwotę do przeliczenia: ");
+            double givenNumber = double.Parse(Console.ReadLine());
+            double result = givenNumber * currency.Rate;
+            Console.WriteLine($"{givenNumber}{currency.CurrencyName} = {Math.Round(result, 2)}{currency.CurrencySymbol}");
+        }
+    }
+}
