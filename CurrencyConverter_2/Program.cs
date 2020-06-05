27bd8de2deab9322e4e7fb2c6945d924455af0fd
kamilwojtysiak <kamil@futrzane.pl>
2020-06-05 11:31:31 +0200

    Zmiana logiki programu

--- CurrencyConverter_2/Program.cs
+++ CurrencyConverter_2/Program.cs
@@ -1,41 +1,67 @@
-﻿using System;
-
-namespace CurrencyConverter_2
+﻿namespace CurrencyConverter_2
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Wybierz walutę, na którą zostanie przeliczona kwota w PLN: ");
+            Console.WriteLine("1.EUR \n2.USD \n3.GBP");
+            Console.Write("Podaj liczbe przypisaną walucie: ");
 
-            Currency EUR = new Currency(0.2255, '€', "PLN");
-            Currency USD = new Currency(0.2527, '$', "PLN");
+            int number = int.Parse(Console.ReadLine());
 
-            CurrencyConverter newConverter = new CurrencyConverter();
-            newConverter.Converter(EUR);
-            newConverter.Converter(USD);
+            if (number == 1)
+            {
+                Currency eur = new Currency(0.2255, '€', "PLN");
+
+                CurrencyConverter converter = new CurrencyConverter();
+                converter.ConvertTo(eur);
+            }
+            else if (number == 2)
+            {
+                Currency usd = new Currency(0.2527, '$', "PLN");
+
+                CurrencyConverter converter = new CurrencyConverter();
+                converter.ConvertTo(usd);
+            }
+            else if (number == 3)
+            {
+                Currency gbp = new Currency(0.2019, '£', "PLN");
+
+                CurrencyConverter converter = new CurrencyConverter();
+                converter.ConvertTo(gbp);
+            }
+            else
+            {
+                Console.WriteLine("Błędnie wprowadzona liczba.");
+            }
         }
     }
+
     public class Currency
     {
         public double Rate { get; set; }
-        public char ExpectedCurrencySymbol { get; set; }
-        public string ConvertedCurrencyName { get; set; }
+        public char Symbol { get; set; }
+        public string Name { get; set; }
 
-        public Currency(double rate, char expectedCurrencySymbol, string convertedCurrencyName)
+        public Currency(double rate, char symbol, string name)
         {
-            this.Rate = rate;
-            this.ExpectedCurrencySymbol = expectedCurrencySymbol;
-            this.ConvertedCurrencyName = convertedCurrencyName;
+            Rate = rate;
+            Symbol = symbol;
+            Name = name;
         }
     }
+
     public class CurrencyConverter
     {
-        public void Converter(Currency currency)
+        public void ConvertTo(Currency currency)
         {
             Console.Write("Wprowadź kwotę do przeliczenia: ");
-            double givenNumber = double.Parse(Console.ReadLine());
-            double result = givenNumber * currency.Rate;
-            Console.WriteLine($"{givenNumber}{currency.ConvertedCurrencyName} = {Math.Round(result, 2)}{currency.ExpectedCurrencySymbol}");
+
+            double amount = double.Parse(Console.ReadLine());
+            double convertedAmount = amount * currency.Rate;
+
+            Console.WriteLine($"{amount}{currency.Name} = {Math.Round(convertedAmount, 2)}{currency.Symbol}");
         }
     }
 }
